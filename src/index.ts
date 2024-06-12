54800d44977fdc512f027a550e96f35c7735bd92
Rafael Sanches <rafaeldss12@gmail.com>
2024-06-11 23:01:20 -0300

    feat/initial working configuration to run express-server

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import { getRanking } from "./services/databaseQueries.ts";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3002;
+
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
+  next();
+});
+
+app.get("/ranking", async (req, res) => {
+  if (typeof req.query.score === "string") {
+    const userScore = parseInt(req.query.score, 10);
+    if (isNaN(userScore)) {
+      return res.status(400).send("Invalid score parameter");
+    }
+    try {
+      const result = await getRanking(userScore);
+      res.status(200).json({ total_count: result.rows[0].total_count });
+    } catch (error) {
+      console.error("Error fetching ranking:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  } else {
+    res.status(400).send("Score parameter is missing or invalid");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
